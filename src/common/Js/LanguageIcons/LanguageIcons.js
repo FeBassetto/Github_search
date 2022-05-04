cef620d48e38d0038526c3e7a6acae49365fb9eb
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-04 08:29:10 -0300

    Color and new icons

--- src/common/Js/LanguageIcons/LanguageIcons.js
+++ src/common/Js/LanguageIcons/LanguageIcons.js
@@ -19,31 +19,80 @@ import {
     SiJupyter,
     SiRust,
     SiKotlin,
-    SiDart
+    SiDart,
+    SiJinja,
+    SiJulia,
+    SiCoffeescript,
+    SiElixir,
+    SiCrystal,
+    SiErlang,
+    SiNim
 
 } from 'react-icons/si'
+import {VscTerminalPowershell} from 'react-icons/vsc'
+import styled from 'styled-components'
+
+const StyledYellow = styled.span`
+    color:#d1b711;
+`
+
+const StyledPurple = styled.span`
+    color:purple;
+`
+
+const StyledBlue = styled.span`
+    color:blue;
+`
+
+const StyledLightblue = styled.span`
+    color:#03b6fc;
+`
+
+const StyledDarkblue = styled.span`
+    color:darkblue;
+`
+
+const StyledRed = styled.span`
+    color:red;
+`
+
+const StyledOrange = styled.span`
+    color:#e38710;
+`
+
+const StyledBlack = styled.span`
+    color:black;
+`
 
 const languagesIcons = {
-    'javascript': <SiJavascript />,
-    'c#': <SiCsharp />,
-    'c++': <SiCplusplus />,
-    'c': <SiCloudera />,
-    'go': <SiGo />,
-    'java': <SiJava />,
-    'php': <SiPhp />,
-    'python': <SiPython />,
-    'scala': <SiScala />,
-    'typescript': <SiTypescript />,
-    'ruby': <SiRuby />,
-    'html': <SiHtml5 />,
-    'css': <SiCss3 />,
-    'perl': <SiPerl />,
-    'r': <SiR />,
-    'jupyter notebook': <SiJupyter />,
-    'rust': <SiRust/>,
-    'kotlin': <SiKotlin/>,
-    'dart': <SiDart/>,
-    'default': <SiGithub />
+    'javascript': <StyledYellow><SiJavascript /></StyledYellow>,
+    'c#': <StyledPurple><SiCsharp /></StyledPurple>,
+    'c++': <StyledBlue><SiCplusplus /></StyledBlue>,
+    'c': <StyledLightblue><SiCloudera /></StyledLightblue>,
+    'go': <StyledLightblue><SiGo /></StyledLightblue>,
+    'java': <StyledRed><SiJava /></StyledRed>,
+    'php': <StyledDarkblue><SiPhp /></StyledDarkblue>,
+    'python': <StyledYellow><SiPython /></StyledYellow>,
+    'scala': <StyledRed><SiScala /></StyledRed>,
+    'typescript': <StyledLightblue><SiTypescript /></StyledLightblue>,
+    'ruby': <StyledRed><SiRuby /></StyledRed>,
+    'html': <StyledOrange><SiHtml5 /></StyledOrange>,
+    'css': <StyledBlue><SiCss3 /></StyledBlue>,
+    'perl': <StyledPurple><SiPerl /></StyledPurple>,
+    'r': <StyledBlue><SiR /></StyledBlue>,
+    'jupyter notebook': <StyledOrange><SiJupyter /></StyledOrange>,
+    'rust': <StyledRed><SiRust /></StyledRed>,
+    'kotlin': <StyledPurple><SiKotlin /></StyledPurple>,
+    'dart': <StyledLightblue><SiDart /></StyledLightblue>,
+    'jinja': <StyledRed><SiJinja/></StyledRed>,
+    'shell': <VscTerminalPowershell/>,
+    'julia': <StyledPurple><SiJulia/></StyledPurple>,
+    'coffeescript': <StyledDarkblue><SiCoffeescript/></StyledDarkblue>,
+    'elixir': <StyledPurple><SiElixir/></StyledPurple>,
+    'crystal':<StyledBlue><SiCrystal/></StyledBlue>,
+    'erlang': <StyledRed><SiErlang/></StyledRed>,
+    'nim':<StyledYellow><SiNim/></StyledYellow>,
+    'default': <StyledBlack><SiGithub /></StyledBlack>
 
 }
 
